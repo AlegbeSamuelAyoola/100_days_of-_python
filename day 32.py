07841ab3ae45ccbd00056949ba82681b7d6272d9
Alegbe Samuel Ayoola <108579288+AlegbeSamuelAyoola@users.noreply.github.com>
2024-11-14 17:45:03 +0100

    Day 32 of the 100 days of code. I created a program the displays "Hello" in a different language anytime it is run.

---
+++ day 32.py
@@ -0,0 +1,17 @@
+import random
+
+def greet():
+    gr = ["Hello there!" #english
+          ,"Hola!" #spanish
+          ,"Ahlaan bik!" #Arabic
+          ,"Bonjur!" #french
+          ,"Henle nibe yen!" #yoruba
+          ]
+    greeting = random.choice(gr)
+    return greeting
+
+title = "====== Hello in Different Languages ======"
+print(f"{title}".center(170))
+print()
+print(f"{greet()}".center(170))
+print()
