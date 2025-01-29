cf2d39b3a898895a83de5d50ec3443b450bd6568
Alegbe Samuel Ayoola <108579288+AlegbeSamuelAyoola@users.noreply.github.com>
2025-01-29 09:47:56 +0100

    Day 77 of the 100 days of code. I created additional webpages on my server.

---
+++ day 77/flask_blog/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/blog1')
+def blog1():
+    return render_template('blog1.html')
+
+@app.route('/blog2')
+def blog2():
+    return render_template('blog2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
